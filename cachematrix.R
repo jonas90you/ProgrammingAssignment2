180e27c2ad68a4470259a20d97a7d2f30481bf76
Jonas <mimouni.y@gmail.com>
2018-04-28 21:49:47 +0100

    Add existing file

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ##initialize mx as NULL
+  mx <- NULL
+  ##define the set function to assign new
+  set <- function(y){
+    ##value of matrix in parent environment
+    x <<- y
+    ##if there is a new matrix, reset inv to NULL
+    mx <<- NULL
+  }
+  get <- function() 
+    
+    setInverse <- function(inverse) inv <<- inverse 
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
 }
